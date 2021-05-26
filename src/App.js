5d8102688e65a1c6d1b04e26f51fea958fb05743
노효근 <81951751+Nrootaroundkorea@users.noreply.github.com>
2021-05-26 17:18:39 +0900

    fix 404 pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Header from './Layout/Header';
-// import Navigation from './Layout/Navigation';
 import Router from './Routes/Router';
 import Store from './Store/store';
 
@@ -31,18 +30,7 @@ onLogout = () => {
     console.log('Goolge Logout.');
     window.sessionStorage.clear();
 }
-  
-  //   const provider = window.sessionStorage.getItem('provider');    
-  //   //Google AccessToken Remove
-  //   if(provider === 'google') {
-  //     const auth2 = window.gapi.auth2.getAuthInstance();
-  //     auth2.signOut().then(function() {
-  //       console.log('Goolge Logout.');
-  //     });
-  //   }
-  //   //SessionStorage Clear
-  //   window.sessionStorage.clear();
-  // }
+
 
   componentDidMount() {
     const id = window.sessionStorage.getItem('id');
@@ -61,7 +49,7 @@ onLogout = () => {
       <Store.Provider value={this.state}>
         <Layout>
           <Header logged={logged} onLogout={onLogout}/>
-          {/* <Navigation /> */}
+
             <Router />
         </Layout>
       </Store.Provider>
